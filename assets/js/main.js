57f380f05b500093df25570800defd8b0c6fcf47
munshid <71083767+Munshidshaz@users.noreply.github.com>
2021-11-23 16:21:12 +0530

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,56 @@
+const navSlide= () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    //toggle nav
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+		burger.classList.toggle('toggle');
+		
+
+
+    });
+	
+
+    
+}
+const autoClose = () =>{
+ const nav = document.querySelector('.nav-links');
+
+	nav.addEventListener('click',() => {
+		nav.classList.toggle('nav-active');
+		
+	});
+}
+
+navSlide();
+autoClose();
+
+(function($) {
+
+	var	$window = $(window),
+		$body = $('body'),
+		$nav = $('#nav');
+
+	// Breakpoints.
+		breakpoints({
+			xlarge:  [ '1281px',  '1680px' ],
+			large:   [ '981px',   '1280px' ],
+			medium:  [ '737px',   '980px'  ],
+			small:   [ null,      '736px'  ]
+		});
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Scrolly.
+		$('#nav a, .scrolly').scrolly({
+			speed: 1000,
+			offset: function() { return $nav.height(); }
+		});
+
+})(jQuery);
